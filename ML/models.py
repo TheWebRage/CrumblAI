ce147afe5b4d039527a68e76929d9291d67f2b95
Daniel Child <danielchild1@mail.weber.edu>
2020-12-17 22:59:42 -0700

    created models and migration files

--- ML/models.py
+++ ML/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Cookie(models.Model):
+
+    def __str__(self):
+        return self.name
+
+    name = models.CharField(max_length=50)
+    attributes = models.ManyToManyField('Attribute')
+    available = models.ManyToManyField('Available')
+
+
+class Attribute(models.Model):
+
+    attribute = models.CharField(max_length=50)
+
+
+class Available(models.Model):
+
+    available = models.DateField()
+
