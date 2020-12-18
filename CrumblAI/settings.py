61b051796264c5be474bb4480d1c3099b23e65de
Daniel Child <danielchild1@mail.weber.edu>
2020-12-17 23:00:16 -0700

    added created apps to the INSTALLED_APPS array

--- CrumblAI/settings.py
+++ CrumblAI/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ML.apps.MlConfig',
+    'Front.apps.FrontConfig',
+
 ]
 
 MIDDLEWARE = [
